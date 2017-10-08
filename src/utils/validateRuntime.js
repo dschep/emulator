63976aae7ba2706bbd760baac1bc0dffb8d1a261
Daniel Schep <dschep@gmail.com>
2017-10-08 13:35:52 -0400

    trying to get python support

--- src/utils/validateRuntime.js
+++ src/utils/validateRuntime.js
@@ -1,5 +1,5 @@
 function validateRuntime(runtime) {
-  if (!runtime.match(/node/)) {
+  if (!runtime.match(/(node|python)/)) {
     throw Error(`Runtime "${runtime}" currently not supported`);
   }
   return runtime;
